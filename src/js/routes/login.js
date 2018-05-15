238819bc0e396a06ad5a163df36dac2b8618507a
holevchuk <holevchuk.evgeny@gmail.com>
2018-05-15 16:42:22 +0300

    add login

--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -1,3 +1,46 @@
 function login() {
 	rootEl.innerHTML = templates.login();
+
+	var form = document.getElementById('login-form');
+
+	var {email, password} = form;
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		if(!isValidMail(email.value)) {
+			email.parentNode.classList.add('has-error');
+			email.nextElementSibling.innerText = 'Email is required';
+		} else {
+			email.parentNode.classList.remove('has-error');
+			email.parentNode.classList.add('has-success');
+			email.nextElementSibling.innerText = '';
+		}
+
+		if(!isValidPassword(password.value)) {
+			password.parentNode.classList.add('has-error');
+			password.nextElementSibling.innerText = 'Password is required';
+		} else {
+			password.parentNode.classList.remove('has-error');
+			password.parentNode.classList.add('has-success');
+			password.nextElementSibling.innerText = '';
+		}
+
+		if(isValidMail(email.value) && isValidPassword(password.value)) {
+			firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+				.then(() => {
+					page('/');
+				})
+		}
+	});
+
+}
+
+function isValidMail(value) {
+	const emailRe = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	return emailRe.test(value);
+}
+
+function isValidPassword(value) {
+	return value.length >= 6;
 }
